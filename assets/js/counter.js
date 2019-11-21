78756c32e1f80dc272eb23f63e13910ffe39e188
Sanchit <sanchitcop19@Gmail.com>
2019-11-20 21:34:04 -0500

    Created counter

---
+++ assets/js/counter.js
@@ -0,0 +1,11 @@
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    var count = document.getElementsByClassName('display-1 text-center')[0];
+    if (evt.keyCode == '38'){
+        count.innerHTML = String(parseInt(count.innerHTML) + 1);
+    }
+    else if (evt.keyCode == '40'){
+        count.innerHTML = String(parseInt(count.innerHTML) - 1);
+    }
+    
+}
